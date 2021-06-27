d57094c8c23e67f3bdd22cbbf511c3a4443eeedd
Ibrahim Ozkan <ibrahim.ozkan@metu.edu.tr>
2021-06-27 23:12:23 +0300

    Added Zombies.h

---
+++ ZombieBusters/Zombies.h
@@ -0,0 +1,16 @@
+#ifndef ZOMBIES_H
+#define ZOMBIES_H
+
+class Zombies {
+private:
+	int dmgPerHit;
+	int scorePerItem;
+public:
+	Zombies() {
+		dmgPerHit = 0;
+		scorePerItem = 0;
+	}
+};
+
+
+#endif
