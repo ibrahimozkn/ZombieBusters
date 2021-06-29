79fd6594c82d6a79d0f98b336dce58da3201c56d
Ibrahim Ozkan <ibrahim.ozkan@metu.edu.tr>
2021-06-29 19:23:50 +0300

    Used Main file for testing

--- ZombieBusters/ZombieBusters.cpp
+++ ZombieBusters/ZombieBusters.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include "Dice.h"
+#include "Entity.h"
+#include "Zombies.h"
+
+using std::cout;
 
 int main()
 {
-		
+	Zombies* z;
+	z = new SmallZombie();
+	cout << z->getLife();
+
+	return 0;
 }
