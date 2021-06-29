11200140d7864452d54441a7e86a3eb6289d5d23
Ibrahim Ozkan <ibrahim.ozkan@metu.edu.tr>
2021-06-29 19:23:11 +0300

    Fixed Entity.h

--- ZombieBusters/Entity.h
+++ ZombieBusters/Entity.h
@@ -7,27 +7,23 @@ class Entity {
 private:
 	char Character;
 	int Size;
-protected:
-	void Entity(char, int);
 public:
-	char get_Character();
-	int get_Size();
+	//constructor (to be called by derived classes)
+
+	Entity(char c, int s) {
+		Character = c;
+		Size = s;
+	}
+
+	//Getters
+
+	char get_Character() {
+		return Character;
+	}
+	int get_Size() {
+		return Size;
+	}
 };
 
-//constructor (to be called by derived classes)
-
-void Entity::Entity(char c, int s) {
-	Character = c;
-	Size = s;
-}
-
-//Getters
-
-char Entity::get_Character() {
-	return Character;
-}
-int Entity::get_Size() {
-	return Size;
-}
 
 #endif
