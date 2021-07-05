70befacaff61294c4e38fa05464a180d870c1217
Ibrahim Ozkan <ibrahim.ozkan@metu.edu.tr>
2021-07-05 13:16:04 +0300

    Started for main function and Added readme.txt

--- ZombieBusters/ZombieBusters.cpp
+++ ZombieBusters/ZombieBusters.cpp
@@ -9,32 +9,95 @@
 #include "Chichonne.h"
 #include "Grid.h"
 using std::cout;
+using std::cin;
 
 int main(){
-	
-	int i;
-	Grid* G = new Grid(7);
-	G->deploy('D', 'C');
-	G->print_grid();
+	srand(time(NULL));
+	cout << "-------------------------------------------------------------------------\n";
+	cout << R"(
+ ______                _     _       ______           _                
+|___  /               | |   (_)      | ___ \         | |               
+   / /  ___  _ __ ___ | |__  _  ___  | |_/ /_   _ ___| |_ ___ _ __ ___ 
+  / /  / _ \| '_ ` _ \| '_ \| |/ _ \ | ___ \ | | / __| __/ _ \ '__/ __|
+./ /__| (_) | | | | | | |_) | |  __/ | |_/ / |_| \__ \ ||  __/ |  \__ \
+\_____/\___/|_| |_| |_|_.__/|_|\___| \____/ \__,_|___/\__\___|_|  |___/
+                                                                       
+                                                                      )";
+	cout << "\n-------------------------------------------------------------------------\n";
+	cout << "\n-> Welcome to ZombieBusters!\n-> Your goal is to clear the island which is raided by the zombies! \n-> The objective of the game is to clean the island together;"
+		<< "however, you can also score and compete with your teammate depending on the number and sizes of the Zombies you kill.\n";
 
-	vector<coordinate> test = G->get_possible_destinations(1);
-	for (i = 0; i < test.size(); i++) {
-		cout << "\n" << test[i].x << ", " << test[i].y << "\n";
+
+	char Player1, Player2;
+	int warriorPicker;
+
+	Grid* island;
+
+	if (chooseDice() == 1) {
+		cout << "\n-> Player 1, choose your warrior \n->1. Derick\n->2. Chichonne\n";
+
+		while (1) {
+			cout << "-> ";
+			cin >> warriorPicker;
+
+			if (warriorPicker == 1) {
+				Player1 = 'D';
+
+				Player2 = 'C';
+				cout << "-> Player1 chose Derick. Player2 plays the game as Chichonne\n";
+				break;
+			}else if (warriorPicker == 2) {
+				Player1 = 'C';
+
+				Player2 = 'D';
+				cout << "-> Player1 chose Chichonne. Player2 plays the game as Derick\n";
+				break;
+			}else {
+				cout << "!> Wrong option! Enter (1) for Derick, (2) for Chichonne\n";
+				continue;
+			}
+		}
+
+
 	}
-	cout << "\n\n";
-	int testx, testy;
-	cin >> testx;
-	cin >> testy;
+	else {
+		cout << "\n-> Player 2, choose your warrior \n->1. Derick\n->2. Chichonne\n";
+		
+		while (1) {
+			cout << "-> ";
+			cin >> warriorPicker;
 
-	cout << "\n\n";
+			if (warriorPicker == 1) {
+				Player1 = 'D';
 
-	G->move_warrior(1, testx, testy);
-	/*for (i = 0; i < G->get_free_neighbors(testx, testy).size(); i++) {
-		cout << G->get_free_neighbors(testx, testy)[i].x << ", " << G->get_free_neighbors(testx, testy)[i].y << "\n";
-	}*/
+				Player2 = 'C';
+				cout << "-> Player2 chose Derick. Player1 plays the game as Chichonne\n";
+				break;
+			}else if (warriorPicker == 2) {
+				Player1 = 'C';
 
-	G->print_grid();
-	
+				Player2 = 'D';
+				cout << "-> Player2 chose Chichonne. Player1 plays the game as Derick\n";
+				break;
+			}
+			else {
+				cout << "!> Wrong option! Enter (1) for Derick, (2) for Chichonne\n";
+				continue;
+			}
+			}
+	}
+
+	int islandSize;
+
+	cout << "\n-> Enter the size of the island that you want to play in (Only enter one side of the nxn island Eg: 5 for 5x5 island): ";
+	cin >> islandSize;
+
+	island = new Grid(islandSize);
+
+
+
+
+
 
 	return 0;
 }
---
+++ ZombieBusters/readme.txt
@@ -0,0 +1,6 @@
+• Used IDE/Compiler name-version: 
+• Changes that you have done to project settings (if any): 
+• Tested Platform (Windows 10, MacOS, Kali, Ubuntu, …): Windows 10
+• Bugs that you have noticed (if any): 
+• Preferred dates with time intervals for demo session (Must be after your final exam): 
+• Comments about your implementation (if any): 
